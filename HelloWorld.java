40eaa361864f0d26911c00d276495d23a6966493
evprado849 <eprado@ucsd.edu>
2022-05-09 11:13:03 -0700

    done

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,9 @@
 public class HelloWorld {
+
+  public static int multiplyTwoInts(int first,int second) {
+    return first*second;
+  }
+  
   public static void main(String[] args) {
     System.out.println("This is my video for CSE15L!");
   }
